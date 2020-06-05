a1bd82cb1839656f300ddea84e81e03c2e67f1df
alugocp <alugocp@aim.com>
2020-06-05 19:45:40 -0400

    Working on compiling Thoth for Android

---
+++ blank/ThothAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Win32.SafeHandles;
+
+namespace Thoth
+{
+
+	internal class API : SafeHandleZeroOrMinusOneIsInvalid
+	{
+		internal API() : base(true){
+		}
+
+		protected override bool ReleaseHandle(){
+
+		}
+	}
+
+}
