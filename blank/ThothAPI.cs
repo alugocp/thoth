6f361fcd54855d22136b4a77632a523ed6927756
alugocp <alugocp@aim.com>
2020-06-10 09:44:40 -0400

    Added language I/O functions to the ABI

--- blank/ThothAPI.cs
+++ blank/ThothAPI.cs
@@ -23,18 +23,39 @@ class ThothAPI:IDisposable{
     }
 	public void new_lang() {
 		wrapped_new_lang();
-	}
+    }
+    public void load_lang(uint l,uint w) {
+        wrapped_load_lang(l,w);
+    }
+    public Tuple<uint, uint> save_lang() {
+        uint l = wrapped_get_lang_seed();
+        uint w = wrapped_get_word_seed();
+        return Tuple.Create(l,w);
+    }
 
 	// Wrapped binary C++ functions
 	private const string DllName = "libthoth.so";
+
 	[DllImport(DllName, EntryPoint = "thothAPI_new_words")]
     [return: MarshalAs(UnmanagedType.LPArray,ArraySubType = UnmanagedType.LPStr,SizeConst = 80)]
     private static extern IntPtr wrapped_new_words(int n,int l);
-	[DllImport(DllName, EntryPoint = "thothAPI_new_language")]
+	
+    [DllImport(DllName, EntryPoint = "thothAPI_new_language")]
 	private static extern void wrapped_new_lang();
-	[DllImport(DllName, EntryPoint = "thothAPI_start")]
+
+    [DllImport(DllName, EntryPoint = "thothAPI_load_language")]
+    private static extern void wrapped_load_lang(uint lseed,uint wseed);
+
+    [DllImport(DllName, EntryPoint = "thothAPI_get_lang_seed")]
+    private static extern uint wrapped_get_lang_seed();
+
+    [DllImport(DllName, EntryPoint = "thothAPI_get_word_seed")]
+    private static extern uint wrapped_get_word_seed();
+
+    [DllImport(DllName, EntryPoint = "thothAPI_start")]
 	private static extern void wrapped_init();
-	[DllImport(DllName, EntryPoint = "thothAPI_free")]
+	
+    [DllImport(DllName, EntryPoint = "thothAPI_free")]
 	private static extern void wrapped_free();
 }
 
